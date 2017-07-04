c9ad8e2dce95e63aee2f729f19b30a4540311dc2
sippad <aasif1311@gmail.com>
2017-07-04 14:42:23 +0530

    Web.xml and linksPage.html done

---
+++ MultipleLinks_SingleServlet/src/edu/hyd/servlet/MyServlet.java
@@ -0,0 +1,49 @@
+package edu.hyd.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String param=null;
+		PrintWriter pw=null;
+		response.setContentType("text/html");
+		param = request.getParameter("p");
+		if(param.equals("countries")){
+			
+		}
+		else if(param.equals("lang")){
+			
+		}
+		else if(param.equals("sysdate")){
+			
+		}								
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
