a199d07d086874e0273bf880c097ac01c1a51123
sippad <aasif1311@gmail.com>
2017-07-09 01:14:39 +0530

    RegEx optimized a little

--- JavaMatcherClassandPattern/src/RegEx.java
+++ JavaMatcherClassandPattern/src/RegEx.java
@@ -2,34 +2,31 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegEx {
-
+	private final static String s1 = "Michael Phelps	23	United States	2008	08-24-08	Swimming	8	0	0	8";
+	private final static String s2 = "Pieter van den Hoogenband	22	Netherlands	2000	10-01-00	Swimming	2	0	2	4";
+	private final static String s3 = "Andrew Lauterstein	21	Australia	2008	08-24-08	Swimming	0	1	2	3";	
+	private static Matcher m1,m2;
+	private static Pattern p1,p2;
 	
 	public static void main(String[] args) {
-		
-		String s1 = "Michael Phelps	23	United States	2008	08-24-08	Swimming	8	0	0	8";
-		String s2 = "Pieter van den Hoogenband	22	Netherlands	2000	10-01-00	Swimming	2	0	2	4";
-		String s3 = "Andrew Lauterstein	21	Australia	2008	08-24-08	Swimming	0	1	2	3";
-		final String REGEX1 = "\\s\\d{2}\\s";
-		final String REGEX2 = "\\s\\d{4}\\s";
-		final String INPUT = "Michael Phelps    23	United States	2008	08-24-08	Swimming	8	0	0	8";
+		int endOfAge=0;
+		int startOfYear=0;
+		String REGEX1 = "\\s\\d{2}\\s";
+		String REGEX2 = "\\s\\d{4}\\s";		
 		 
-		      Pattern p1 = Pattern.compile(REGEX1);
-		      Pattern p2 = Pattern.compile(REGEX2);
-		      Matcher m1 = p1.matcher(s2);   // get a matcher object
-		      Matcher m2 = p2.matcher(s2);   // get a matcher object
-		      int endOfAge=0,startOfYear=0;
-		      int count = 0;
-
-		      while(m1.find()) {		           		       
-		         endOfAge=m1.end();		    
-		      }
-		      
-		      while(m2.find()){
-		    	startOfYear = m2.start();
-		      }
-		      
-		      System.out.println("End of Age : "+endOfAge);
-		      System.out.println("Start of Year : "+startOfYear);
-		      System.out.println(s2.substring(endOfAge, startOfYear));
+		p1 = Pattern.compile(REGEX1);
+		p2 = Pattern.compile(REGEX2);
+		
+		m1 = p1.matcher(s1);
+		m2 = p2.matcher(s1); 
+				
+		while(m1.find()){ 		           		       
+		    endOfAge=m1.end();
+		    if(m2.find())	
+		    	startOfYear=m2.start();
+		}		      		      
+		System.out.println("End of Age : "+endOfAge);
+		System.out.println("Start of Year : "+startOfYear);
+		System.out.println(s1.substring(endOfAge, startOfYear));
 	}
 }
