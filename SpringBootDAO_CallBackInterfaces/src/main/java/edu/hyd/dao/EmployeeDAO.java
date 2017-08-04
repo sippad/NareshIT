3ce0f09e212045ed3edafbe0c43e3cf096d93064
sippad <aasif1311@gmail.com>
2017-08-04 21:32:10 +0530

    interface EmployeeDAO commit

--- SpringBootDAO_CallBackInterfaces/src/main/java/edu/hyd/dao/EmployeeDAO.java
+++ SpringBootDAO_CallBackInterfaces/src/main/java/edu/hyd/dao/EmployeeDAO.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import edu.hyd.bo.EmployeeBO;
 
-public interface EmployeeDAO {
-	
-	public List<EmployeeBO> getAllEmpDetails() throws Exception; 
-	
+public interface EmployeeDAO {	
+	public List<EmployeeBO> getAllEmpDetails() throws Exception; 	
 }
